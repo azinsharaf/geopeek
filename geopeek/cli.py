3181f03308bcb10376efc839fdbec916540eed9a
Azin Sharaf <azin.sharaf@icloud.com>
2025-09-09 22:36:26 -0700

    fix: Update CLI to use rich table for info output.

--- geopeek/cli.py
+++ geopeek/cli.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 from typer import Argument, Typer
 from geopeek.output.rich_printer import print_rich_table  # Corrected import statement
+from geopeek.handlers.gdb_handler import GDBHandler
 
 console = Console()
 
@@ -10,7 +11,7 @@ app = Typer()
 def info(input_file: str):
     """Print information about the input file"""
     handler = GDBHandler(input_file)
-    handler.print_info()
+    handler.print_rich_table()
 
 if __name__ == '__main__':
     app()
