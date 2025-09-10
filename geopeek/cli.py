e4486a5b9c6fb206382cf3ef50605f20fc3c4634
Azin Sharaf <azin.sharaf@icloud.com>
2025-09-09 22:41:21 -0700

    fix: Use print_info instead of print_json for GDBHandler.

--- geopeek/cli.py
+++ geopeek/cli.py
@@ -1,7 +1,7 @@
 from rich.console import Console
 from typer import Argument, Typer
 from geopeek.output.json_printer import print_json  # Import the correct function
-
+from geopeek.handlers.gdb_handler import GDBHandler
 console = Console()
 
 app = Typer()
@@ -10,4 +10,4 @@ app = Typer()
 def info(input_file: str):
     """Print information about the input file"""
     handler = GDBHandler(input_file)
-    handler.print_json()  # Update this line to use the correct function
+    handler.print_info()  # Update this line to use the correct function
