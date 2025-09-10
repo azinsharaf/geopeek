35fbe9bb3cc8a6abd6b60f4cc6ac9f9a64b7acfc
Azin Sharaf <azin.sharaf@icloud.com>
2025-09-09 22:08:03 -0700

    feat: Implement basic CLI for geopeek application.

--- geopeek/cli.py
+++ geopeek/cli.py
@@ -1,1 +1,15 @@
+import argparse
+from geopeek.core import main
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Geopeek CLI')
+    parser.add_argument('input_file', help='Input file path')
+    return parser.parse_args()
+
+def main_func(args):
+    # Call the main function from core module
+    main.main(args.input_file)
+
+if __name__ == '__main__':
+    args = parse_args()
+    main_func(args)
