b8c6b120bbc7d4532b2feb2fcfb8644feee68f5b
Azin Sharaf <azin.sharaf@icloud.com>
2026-05-14 23:24:39 -0700

    chore: use Join-Path for sample_data in dev.ps1

--- dev.ps1
+++ dev.ps1
@@ -1,3 +1,3 @@
 $env:TEXTUAL = "debug,devtools"
-python -m geopeek browse $PSScriptRoot/sample_data/
+python -m geopeek browse (Join-Path $PSScriptRoot "sample_data")
 Remove-Item Env:TEXTUAL -ErrorAction SilentlyContinue
