d98066d3b12ff6007ccf189d3f3a0d474e319392
Azin Sharaf <azin.sharaf@icloud.com>
2026-05-14 23:22:13 -0700

    chore: fix sample_data path to geopeek/sample_data/

--- dev.ps1
+++ dev.ps1
@@ -1,3 +1,3 @@
 $env:TEXTUAL = "debug,devtools"
-python -m geopeek browse $PSScriptRoot/sample_data/
+python -m geopeek browse $PSScriptRoot/geopeek/sample_data/
 Remove-Item Env:TEXTUAL -ErrorAction SilentlyContinue
